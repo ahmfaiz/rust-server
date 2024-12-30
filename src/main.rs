cb3ae734edb0c62f0afd4342bd74ad61e36fba92
Faiz Ahmed <faiz227@proton.me>
2024-12-30 18:31:51 +0530

    Printing requests

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream}
+};
 
 fn main() {
     let socket = TcpListener::bind("127.0.0.1:9001").unwrap();
 
     for stream in socket.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
 
         println!("Connection recieved");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buffer = BufReader::new(&stream);
+    let http_request: Vec<_> = buffer
+    .lines()
+    .map(|result| result.unwrap())
+    .take_while(|line| !line.is_empty())
+    .collect();
+
+    println!("Request recieved: {http_request:#?}");
 }
